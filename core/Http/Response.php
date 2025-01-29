bb33b40238ad1cc2b637be6d75d089b2dc9e3061
eliseekn <eliseekn@gmail.com>
2025-01-29 14:11:36 +0000

    fix: response back url

--- core/Http/Response.php
+++ core/Http/Response.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * @copyright (2019 - 2024) - N'Guessan Kouadio Elisée (eliseekn@gmail.com)
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
  * @license MIT (https://opensource.org/licenses/MIT)
  * @link https://github.com/eliseekn/tinymvc
  */
@@ -13,19 +15,21 @@ use Core\Exceptions\InvalidJsonDataException;
 use Core\Exceptions\InvalidResponseDataException;
 
 /**
- * Send HTTP response
+ * Send HTTP response.
  */
 class Response
 {
     public function __construct(
-        public string $uri = '', 
+        public string $uri = '',
         public $data = null,
         public array $headers = []
-    ) {}
+    ) {
+    }
 
     public function addHeaders(array $headers): self
     {
         $this->headers = array_merge($this->headers, $headers);
+
         return $this;
     }
 
@@ -44,6 +48,7 @@ class Response
     public function view(string $view, array $data = []): self
     {
         $this->data = view($view, $data);
+
         return $this;
     }
 
@@ -51,16 +56,16 @@ class Response
     {
         $query = '';
 
-        if (!empty($queries)) {
+        if (! empty($queries)) {
             $query .= '?' . http_build_query($queries);
         }
 
-        $this->uri = !session()->has('intended') ? $uri : session()->pull('intended');
+        $this->uri = ! session()->has('intended') ? $uri : session()->pull('intended');
         $this->addHeaders(['Location' => url($this->uri . rawurldecode($query))]);
 
         return $this;
     }
-    
+
     public function route(string $route, $params = null): self
     {
         return $this->url(route_uri($route, $params));
@@ -70,14 +75,19 @@ class Response
     {
         $history = session()->get('history');
 
-        if(empty($history)) {
+        if (empty($history)) {
             return $this->url('/');
         }
 
         end($history);
+
+        if (prev($history) === false) {
+            return $this->url('/');
+        }
+
         return $this->url(prev($history));
     }
-    
+
     public function intended(string $uri): self
     {
         return $this->with('intended', $uri);
@@ -86,52 +96,54 @@ class Response
     public function with(string $key, $data): self
     {
         session()->create($key, $data);
+
         return $this;
     }
-    
+
     public function withErrors(array $errors): self
     {
-        session()->create('errors', $errors);
-        return $this;
+        return $this->with('errors', $errors);
     }
 
     public function withInputs(array $inputs): self
     {
-        session()->create('inputs', $inputs);
-        return $this;
+        return $this->with('inputs', $inputs);
     }
-    
+
     public function withCookie(string $name, string $value, int $expire = 3600, bool $secure = false, string $domain = ''): self
     {
         cookies()->create($name, $value, $expire, $secure, $domain);
+
         return $this;
     }
 
     public function withoutCookie(array|string $names): self
     {
         cookies()->delete($names);
+
         return $this;
     }
 
     public function download(string $filename): self
     {
-        if (!file_exists($filename)) {
+        if (! file_exists($filename)) {
             throw new FileNotFoundException($filename);
         }
 
         $this->addHeaders([
             'Content-Type' => mime_content_type($filename),
             'Content-Length' => filesize($filename),
-			'Content-Disposition' => 'attachment; filename="' . basename($filename) . '"',
-			'Cache-Control' => 'no-cache',
-			'Pragma' => 'no-cache',
-            'Expires' => '0'
+            'Content-Disposition' => 'attachment; filename="' . basename($filename) . '"',
+            'Cache-Control' => 'no-cache',
+            'Pragma' => 'no-cache',
+            'Expires' => '0',
         ]);
 
         ob_clean();
         flush();
 
         $this->data = readfile($filename);
+
         return $this;
     }
 
@@ -149,6 +161,7 @@ class Response
         ]);
 
         $this->data = $data;
+
         return $this;
     }
 
@@ -164,7 +177,7 @@ class Response
             header($key . ':' . $value);
         }
 
-        if (!empty($this->data)) {
+        if (! empty($this->data)) {
             exit($this->data);
         }
 
